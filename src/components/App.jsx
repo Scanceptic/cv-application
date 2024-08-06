b25a5d81cb7d444a7d118714e3c9113cfd998873
Nathan <urwin.nathan@gmail.com>
2024-08-06 16:58:19 +0100

    expand inputs and add in Todo item comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,24 @@ import General from './General'
 import Education from './Education'
 import Work from './Work'
 
+// Create Edit Button
+
+// Create Submit Button
+
+
 const App = () => {
     return (
         <>
             <h1>CV Application Builder</h1>
+            {/* When edit is clicked, set editing to true */}
             <button>Edit</button>
+            {/* if editing === false, disable input editing */}
             <div id='container'>
                 <General />
                 <Education />
                 <Work />
             </div>
+            {/* When submit is clicked, set editing to false */}
             <button>Submit</button>
         </>
     )
